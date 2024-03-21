6874a600ecd2e095bf5c03dc0579bb6c36cb5bf0
theHoracle <princemmjames2@gmail.com>
2024-03-21 14:28:47 +0100

    trobleshooting

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -31,6 +31,16 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     });
   }, [swiper, urls]);
 
+  const loaderProp = ({ src }: { src: string }) => {
+    const regex = /\./;
+    if (regex.test(src)) {
+      return src;
+    }
+    src =
+      "https://github.com/joschan21/digitalhippo/blob/master/public/thumbnail.jpg";
+    return src;
+  };
+
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 
@@ -90,6 +100,7 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
               src={url}
               alt="Product Image"
               className="-z-10 h-full w-full object-center object-cover"
+              loader={loaderProp}
             />
           </SwiperSlide>
         ))}
